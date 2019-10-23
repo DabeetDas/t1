11384e695041d495514aa871028c4e732d5e2512
DabeetDas <46596266+DabeetDas@users.noreply.github.com>
2019-10-23 20:50:07 +0530

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -14,7 +14,7 @@ module.exports = {
 	 * @desc Default bot timezone if server's location is unknown
 	 * @default "Australia/Melbourne"
 	 */
-	zone: "Asia/Tokyo",
+	zone: "Australia/Melbourne",
 	/** 
 	 * @desc Default bot name format
 	 * @default "ddd h:mm A"
